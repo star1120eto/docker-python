94e495ded80432fd31c2483027dc5ffa3d3d870f
star1120eto <etoile.letoile@gmail.com>
2024-07-01 23:32:28 +0000

    add棒グラフの出力

--- src/chapter2-4-array-sum.py
+++ src/chapter2-4-array-sum.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 monk_fish_team = [158, 157,163,157,145]
 len_fish = len(monk_fish_team)
 sum_fish = sum(monk_fish_team)
@@ -10,3 +12,7 @@ print(min_fish)
 
 mean_fish = sum_fish/len_fish
 print(mean_fish)
+
+plt.bar([0,1,2,3,4], monk_fish_team)
+plt.plot([0,len_fish],[mean_fish, mean_fish], color = 'red')
+plt.show()
