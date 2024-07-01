26b6ebb09c988f883f058495f122e1d66ea7e631
star1120eto <etoile.letoile@gmail.com>
2024-07-01 23:33:07 +0000

    update:フォーマッター実行

--- src/chapter2-4-array-sum.py
+++ src/chapter2-4-array-sum.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-monk_fish_team = [158, 157,163,157,145]
+monk_fish_team = [158, 157, 163, 157, 145]
 len_fish = len(monk_fish_team)
 sum_fish = sum(monk_fish_team)
 max_fish = max(monk_fish_team)
@@ -10,9 +10,9 @@ print(sum_fish)
 print(max_fish)
 print(min_fish)
 
-mean_fish = sum_fish/len_fish
+mean_fish = sum_fish / len_fish
 print(mean_fish)
 
-plt.bar([0,1,2,3,4], monk_fish_team)
-plt.plot([0,len_fish],[mean_fish, mean_fish], color = 'red')
+plt.bar([0, 1, 2, 3, 4], monk_fish_team)
+plt.plot([0, len_fish], [mean_fish, mean_fish], color="red")
 plt.show()
