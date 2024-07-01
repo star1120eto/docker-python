de7b2509426ba0149c4eb692a8b3b38526db41e8
star1120eto <etoile.letoile@gmail.com>
2024-07-01 23:29:32 +0000

    配列の操作（長さ、合計、最大、最小）

---
+++ src/chapter2-4-array-sum.py
@@ -0,0 +1,12 @@
+monk_fish_team = [158, 157,163,157,145]
+len_fish = len(monk_fish_team)
+sum_fish = sum(monk_fish_team)
+max_fish = max(monk_fish_team)
+min_fish = min(monk_fish_team)
+
+print(sum_fish)
+print(max_fish)
+print(min_fish)
+
+mean_fish = sum_fish/len_fish
+print(mean_fish)
