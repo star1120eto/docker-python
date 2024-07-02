0d7b58ed8986447b4e3f3e75c6045a48ad0d7352
star1120eto <etoile.letoile@gmail.com>
2024-07-02 11:20:16 +0000

    update

--- src/chapter2-5_for.py
+++ src/chapter2-5_for.py
@@ -7,12 +7,15 @@ for human in hsn:
 monk_fish_team = [158, 157, 163, 157, 145]
 total = sum(monk_fish_team)
 length = len(monk_fish_team)
-mean = total/length
+mean = total / length
 
 variance = 0
 
 for height in monk_fish_team:
-    variance += (height-mean)**2
-    
-variance = variance/length
+    variance += (height - mean) ** 2
+
+variance = variance / length
 print(variance)
+
+# 標準偏差
+print(variance**0.5)
