bb6b1fc44e10108c9c1c732a2ea01e7c0a472e5a
star1120eto <etoile.letoile@gmail.com>
2024-07-02 11:30:08 +0000

    fix:Lint

--- src/chapter2-5_range.py
+++ src/chapter2-5_range.py
@@ -1,12 +1,12 @@
 for cnt in range(10):
     print(cnt)
-    
+
 for cnt2 in range(73, 98):
     print(cnt2)
-    
+
 # 複利計算
-savings = 100 #元金
+savings = 100  # 元金
 for i in range(15):
-    savings += savings*0.05
+    savings += savings * 0.05
 
 print(savings)
