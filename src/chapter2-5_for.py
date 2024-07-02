008f44eed735d199d6b15c32039fd0c1d2fa6f94
star1120eto <etoile.letoile@gmail.com>
2024-07-02 11:08:57 +0000

    update : 分数の計算

---
+++ src/chapter2-5_for.py
@@ -0,0 +1,18 @@
+# リストをforでprintする
+hsn = ["ゆう", "ひと", "じん", "よう", "みう"]
+for human in hsn:
+    print(human)
+
+# 分数の計算
+monk_fish_team = [158, 157, 163, 157, 145]
+total = sum(monk_fish_team)
+length = len(monk_fish_team)
+mean = total/length
+
+variance = 0
+
+for height in monk_fish_team:
+    variance += (height-mean)**2
+    
+variance = variance/length
+print(variance)
