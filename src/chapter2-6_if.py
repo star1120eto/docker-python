4a4de7fe7cd137727909260054f620af189fabc7
star1120eto <etoile.letoile@gmail.com>
2024-07-02 11:53:38 +0000

    update:数値、文字の比較

--- src/chapter2-6_if.py
+++ src/chapter2-6_if.py
@@ -1,7 +1,30 @@
+# どれがTrueか
 if 2 * 2 * 2 + 2 == 10:
     print("2*2*2+2は10")
 if 2 + 2 * 2 + 2 == 10:
-    print("2+2*2+2は10") # これは8だからfalse
+    print("2+2*2+2は10")  # これは8だからfalse
 if (2 + 2) * 2 + 2 == 10:
     print("(2+2)*2+2は10")
 
+# 数値の比較
+if 1 == 1:
+    print("1番目はTrue")
+
+if 5 ^ (4 - 4) + 9 == 10:
+    print("2番目はTrue")
+
+if 2 < len([0, 1, 2]):
+    print("3番目はTrue")
+
+if sum([1, 2, 3, 4]) < 10:
+    print("4番目はTrue")
+
+# 文字列の比較
+if "AUG" == "AUG":
+    print("1番目はTrue")
+
+if "AUG" == "aug":
+    print("2番目はTrue")
+
+if "あいう" == "あいう":
+    print("3番目はTrue")
