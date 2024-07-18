8a88c8ac0a7c9da4ad6232b345fa28ab7956ebd2
star1120eto <etoile.letoile@gmail.com>
2024-07-18 22:58:02 +0000

    update: random追加

--- src/2-7_def.py
+++ src/2-7_def.py
@@ -1,4 +1,7 @@
-def destiny_tanks2(num):
+from random import randint
+
+
+def destiny_human2(num):
     stars = ['勇太','仁美','迅','耀']
     idx = num % len(stars)
     print("あなたの運命の相手は")
@@ -6,4 +9,8 @@ def destiny_tanks2(num):
     
 num_str = input('あなたの好きな数字は？：')
 num = int(num_str)
-destiny_tanks2(num)
+destiny_human2(num)
+
+num_rand = randint(0, 10)
+print("ランダム")
+destiny_human2(num_rand)
