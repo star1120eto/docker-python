3a068483affb27374489e390b56f57455dacb110
star1120eto <etoile.letoile@gmail.com>
2024-07-18 23:08:56 +0000

    update: returnの追加

--- src/2-7_def.py
+++ src/2-7_def.py
@@ -7,6 +7,7 @@ def destiny_human2(num):
     print("あなたの運命の相手は")
     print(stars[idx])
 
+
 num_str = input("あなたの好きな数字は？：")
 num = int(num_str)
 destiny_human2(num)
@@ -14,3 +15,16 @@ destiny_human2(num)
 num_rand = randint(0, 10)
 print("ランダム")
 destiny_human2(num_rand)
+
+
+def destiny_human3(num):
+    stars = ["勇太", "仁美", "迅", "耀"]
+    idx = num % len(stars)
+    return stars[idx]
+
+
+print("ランダム + return")
+num_rand = randint(0, 10)
+star = destiny_human3(num_rand)
+print("今日あなたと出会う可能性のある人は", star, "です。")
+print("出会う確率は", num_rand * 10, "％です。")
