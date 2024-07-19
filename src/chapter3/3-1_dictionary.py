65840d4d966062bfd6c6158f147daa38b490381f
star1120eto <etoile.letoile@gmail.com>
2024-07-19 20:31:31 +0000

    add:辞書型の使い方

---
+++ src/chapter3/3-1_dictionary.py
@@ -0,0 +1,20 @@
+star = {
+    "ニックネーム":"ほっしー",
+    "出身地":"ほぼ静岡",
+    "最終学歴":"小学校"
+}
+print(star["出身地"])
+
+star["出身地"] = "神奈川県"
+print(star["出身地"])
+
+star["年齢"] = "12歳"
+print(star["年齢"])
+print(star)
+
+del star["最終学歴"]
+print(star)
+
+# エラーになるのでコメントアウト
+# del star["学歴"]
+# print(star)
