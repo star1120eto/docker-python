c501bef2fa261bd04a79a9d00b4c3b4323a35ebd
Yuta <etoile.letoile@gmail.com>
2024-07-21 08:23:36 +0000

    update: ローマ数字変換

--- src/chapter3/3-1_dictionary.py
+++ src/chapter3/3-1_dictionary.py
@@ -1,8 +1,4 @@
-star = {
-    "ニックネーム":"ほっしー",
-    "出身地":"ほぼ静岡",
-    "最終学歴":"小学校"
-}
+star = {"ニックネーム": "ほっしー", "出身地": "ほぼ静岡", "最終学歴": "小学校"}
 print(star["出身地"])
 
 star["出身地"] = "神奈川県"
@@ -18,3 +14,28 @@ print(star)
 # ＫeyError: '学歴' になるので注意
 # del star["学歴"]
 # print(star)
+
+
+def convert_arabia_number(num):
+    roman_nums = {
+        1: "Ⅰ",
+        2: "Ⅱ",
+        3: "Ⅲ",
+        4: "Ⅳ",
+        5: "Ⅴ",
+        6: "Ⅵ",
+        7: "Ⅶ",
+        8: "Ⅷ",
+        9: "Ⅸ",
+        10: "Ⅹ",
+    }
+
+    if num in roman_nums:
+        return roman_nums[num]
+    else:
+        return "[変換できません]"
+
+
+print(convert_arabia_number(1))
+print(convert_arabia_number(8))
+print(convert_arabia_number(99))
